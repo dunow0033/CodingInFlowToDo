1dac389ef10f04d8f58403fbdeea98ad87bc7d38
Brian Donnoe <dunow33@hotmail.com>
2021-10-31 11:12:53 -0400

    finished video 2

--- app/src/main/java/com/example/codinginflowtodo/data/Task.kt
+++ app/src/main/java/com/example/codinginflowtodo/data/Task.kt
@@ -1,14 +1,19 @@
 package com.example.codinginflowtodo.data
 
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
 
+@Entity(tableName = "task_table")
 @Parcelize
 data class Task (
    val name: String,
    val important: Boolean = false,
    val completed: Boolean = false,
    val created: Long = System.currentTimeMillis()
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
  ) : Parcelable {
-
+    val createdDateFormatted: String
+        get() = DateFormat.getDateInstance().format(created)
 }
