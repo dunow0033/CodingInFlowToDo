78ddf0faae0cc978fe36481d8bfe384071d854af
Brian Donnoe <dunow33@hotmail.com>
2021-11-18 14:15:14 -0500

    adding Hilt with database stuff, finished video 4

--- app/src/main/java/com/example/codinginflowtodo/data/TaskDatabase.kt
+++ app/src/main/java/com/example/codinginflowtodo/data/TaskDatabase.kt
@@ -2,10 +2,37 @@ package com.example.codinginflowtodo.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.example.codinginflowtodo.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(
     entities = [Task::class],
-    version
+    version = 1
 )
 abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+
+            applicationScope.launch {
+                dao.insert(Task("Wash the dishes"))
+                dao.insert(Task("Wash the dishes2"))
+                dao.insert(Task("Wash the dishes3", important = true))
+                dao.insert(Task("Wash the dishes4", completed = true))
+                dao.insert(Task("Wash the dishes5", completed = true))
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/codinginflowtodo/ui/tasks/TasksViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.codinginflowtodo.ui.tasks
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.example.codinginflowtodo.data.TaskDao
+
+class TasksViewModel @ViewModelInject constructor(
+    private val taskDao: TaskDao
+) : ViewModel() {
+}
