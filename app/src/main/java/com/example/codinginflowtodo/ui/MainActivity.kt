0ef5c0b62a4c90d542b741e1762242d1733e1cad
Brian Donnoe <dunow33@hotmail.com>
2021-11-17 23:52:28 -0500

    finished video 3, did some nav_graph work and some work on Tasks Fragment

--- app/src/main/java/com/example/codinginflowtodo/ui/MainActivity.kt
+++ app/src/main/java/com/example/codinginflowtodo/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.codinginflowtodo
+package com.example.codinginflowtodo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.codinginflowtodo.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/codinginflowtodo/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.example.codinginflowtodo.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.codinginflowtodo.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+}
