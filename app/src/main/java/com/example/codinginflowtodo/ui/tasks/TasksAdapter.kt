8336975dab032742c3f3d98cff612a7379d2143e
Brian Donnoe <dunow33@hotmail.com>
2021-11-18 19:08:09 -0500

    created viewmodel and adapter for recycler view, finished video 5

---
+++ app/src/main/java/com/example/codinginflowtodo/ui/tasks/TasksAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.codinginflowtodo.ui.tasks
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.codinginflowtodo.data.Task
+import com.example.codinginflowtodo.databinding.ItemTaskBinding
+
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TasksViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TasksViewHolder {
+        return TasksViewHolder(
+            ItemTaskBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false)
+            )
+    }
+
+    override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class TasksViewHolder(private val binding: ItemTaskBinding) :RecyclerView.ViewHolder(binding.root) {
+        fun bind(task: Task) {
+            binding.apply {
+                checkBoxCompleted.isChecked = task.completed
+                textViewName.text = task.name
+                textViewName.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
+            }
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/example/codinginflowtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/codinginflowtodo/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package com.example.codinginflowtodo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.codinginflowtodo.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
